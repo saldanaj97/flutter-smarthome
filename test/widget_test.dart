15bed97a04092cf674a8771cb10a392dc4314b23
Juan Saldana <saldanaj97@gmail.com>
2023-12-19 15:01:24 -0600

    Update app title and import path

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:smarthome/main.dart';
+import 'package:smarthome/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
